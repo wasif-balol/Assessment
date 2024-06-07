827e04a4fa9d683ad8d520dca2140480dda4065a
Wasif Balol <wasif.balol1@gmail.com>
2024-06-07 19:53:06 +0500

    Added User model in admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'is_active', 'is_admin',)
+    readonly_fields = ('is_active', 'is_admin',)
+
+
+admin.site.register(User, UserAdmin)
